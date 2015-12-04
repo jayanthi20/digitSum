219593acd8aa39c99cea3ba26c01cc42ddae3380
jayanthi20 <jayanthir199@gmail.com>
2015-12-04 19:33:45 +0530

    deploy check

--- util/digitSum.js
+++ util/digitSum.js
@@ -2,7 +2,7 @@ var digitSum = function(number,cb){
 	if ( isNaN(number) || number % 1 != 0){
 		return cb('Invalid Input',null)
 	} else {
-		var str = number.toString().split("")
+		var str = number.toString()//.split("")
 		//var sum = str.reduce((prev, curr) => parseInt(prev) + parseInt(curr))
 		var sum = 0;
 		for (var i = 0; i < str.length; i++) {
